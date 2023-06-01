969f548f9f7e47e4e4626d91cb1eaa5138c75a7c
JJ Möller <juanmoller.jm@gmail.com>
2023-06-02 00:59:58 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="signature">
+    <img src="https://styleguide.euromonitor.com/assets/images/brand-guide/passport/the-signature/vertical-lockup.png">
+  </div>
+  <h1>Price of Converse Chuck Taylor All Star High School</h1>
+  <span class="subheading">Graphed over a time period from 2015-2016</span>
+  <span class="subheading">Price depicted is the average between three retailers</span>
+  <div id="chart-container">
+    <Line :data="data" :options="options" />
+  </div>
+  <div class="author">
+    <span>Visualisation by Juan Moller</span>
+  </div>
+</template>
+
+<script lang="ts">
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+import { Line } from 'vue-chartjs'
+import * as chartConfig from './components/Chart.vue'
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+)
+
+export default {
+  name: 'App',
+  components: {
+    Line
+  },
+  data() {
+    return chartConfig
+  }
+}
+</script>
+
+<scss>
+  body {
+    display: absolute;
+    margin: auto;
+
+    #app {
+      display: flex;
+      flex-direction: column;
+
+      .signature {
+        position: fixed;
+        top: 2rem;
+        left: 3rem;
+
+        img {
+          width: 128px;
+          height: auto;
+        }
+      }
+
+      .subheading {
+        &:nth-of-type(2) {
+         margin-bottom: 1em;
+         color: #787878;
+        }
+      }
+
+      #chart-container {
+        max-width: 800px;
+        height: 400px;
+      }
+
+      .author {
+        position: relative;
+        top: 2rem;
+        right: 0;
+        text-align: right;
+        color: #787878;
+      }
+
+      @media (max-width: 1200px) {
+        .signature {
+          position: static;
+          margin: 0 auto;
+          padding-bottom: 1rem;
+        }
+      }
+    }
+  }
+</scss>
---
+++ src/components/Chart.vue
@@ -0,0 +1,54 @@
+<script>
+export const data = {
+  labels: ['2015', '2016', '2017', '2018', '2019'],
+  datasets: [
+    {
+      label: 'All Star Low Tops',
+      backgroundColor: '#34657f',
+      borderColor: '#34657f',
+      data: [595, 607, 650, 667, 669]
+    },
+    {
+      label: 'All Star High Tops',
+      backgroundColor: '#00aed9',
+      borderColor: '#00aed9',
+      data: [644, 702, 746, 737, 749]
+    }
+  ]
+}
+export const options = {
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      position: 'right' // Display the legend on the right side
+    }
+  },
+  scales: {
+    y: {
+      grid: {
+        display: true, // Hide the Y-axis grid lines
+        drawBorder: false // Hide the Y-axis line
+      },
+
+      ticks: {
+        maxTicksLimit: 5,
+        Max: 800, // Specify the maximum value for the Y-axis labels
+        suggestedMin: 550, // Specify the minimum value for the Y-axis labels
+        precision: 0, // Set the number of decimal places for the tick labels
+
+        callback: function(value, index, values) {
+          return 'R' + value; // Add a dollar sign in front of each Y-axis value
+        }
+      }
+    },
+    x: {
+      grid: {
+        borderDash: [3, 3], 
+        display: false, // Hide the Y-axis grid lines
+        drawBorder: false // Hide the Y-axis line
+      },
+    }
+  }
+};
+</script>
